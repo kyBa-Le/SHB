ce2d38a6d109178bade6a3ba88428249f65b896f
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-08 09:58:55 +0700

    3 | done col 1 of footer - kyba

--- public/views/components/footer.php
+++ public/views/components/footer.php
@@ -1,2 +1,45 @@
 <?php ?>
-<h1>This is footer</h1>
+<style>
+    #container-footer {
+        background-color: #0F0E0E;
+    }
+    #container-footer * {
+        color: white;
+    }
+    #footer-logo {
+        width: 10%;
+        height: auto;
+    }
+</style>
+<div class="container-fluid" id="container-footer">
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col d-flex justify-content-start align-items-center" onclick="{window.location.href='/'}" style="cursor: pointer">
+                    <img id="footer-logo" src="images/logo.png">
+                    <h2 class="fw-bold m-0">SHB STORE</h2>
+                </div>
+            </div>
+            <div class="row">
+                <p>We always appreciate and look forward to receiving</p>
+                <p>all feedback from customers to be able to upgrade</p>
+                <p>our service and product experience even better.</p>
+            </div>
+            <div class="row d-flex justify-content-around" style="max-width: 80%">
+                <i class="fa-brands fa-facebook-f col text-center fs-3"></i>
+                <i class="fa-brands fa-square-instagram col text-center fs-3"></i>
+                <i class="fa-brands fa-twitter col text-center fs-3"></i>
+                <i class="fa-brands fa-linkedin-in col text-center fs-3"></i>
+            </div>
+        </div>
+        <div class="col">
+            Column
+        </div>
+        <div class="col">
+            Column
+        </div>
+        <div class="col">
+            Column
+        </div>
+    </div>
+</div>
