c39e307e9848f53d54ae84508a7d394ab7c30500
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-08 09:18:50 +0700

    3 | add footer to main layout - kyba

---
+++ public/views/components/footer.php
@@ -0,0 +1,2 @@
+<?php ?>
+<h1>This is footer</h1>
--- public/views/layouts/main.php
+++ public/views/layouts/main.php
@@ -12,6 +12,7 @@
 <body>
 <div>Header before login</div>
  {{content}}
+<?php require_once 'views/components/footer.php'?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
