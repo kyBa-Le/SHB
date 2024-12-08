c8f53d93a662f5e4d4893f3df12072cf755f1e66
kimhien0711 <hiencaok@gmail.com>
2024-12-08 21:45:09 +0700

    3 | create table products again - kimhien

--- SHB_SQL_Script.sql
+++ SHB_SQL_Script.sql
@@ -1,14 +1,11 @@
 create database SHB;
 use SHB;
-create table Users (
+create table Products (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    fullName VARCHAR(50),
-    phone VARCHAR(10),
-    avatar_link VARCHAR(255),
-    province VARCHAR(255),
-    district VARCHAR(255),
-    detailed_address VARCHAR(255)
+    product_name VARCHAR(255) NOT NULL, 
+    category ENUM('MEN', 'WOMEN', 'BABY') NOT NULL,
+    price INT NOT NULL,
+    image_link VARCHAR(255),
+    purchases INT DEFAULT 0, 
+    quantity INT NOT NULL
 );
