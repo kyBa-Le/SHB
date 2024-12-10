cbb398b6ca796422e6aaf2aa18b69e2cd1ce9683
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-10 16:11:41 +0700

    4 | apply DI to Site Controller - kyba

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -3,20 +3,27 @@
 namespace app\controller;
 
 use app\core\Controller;
+use app\core\Request;
 
 class SiteController extends Controller
 {
+    private $productController;
+    private $userController;
+
+    public function __construct() {
+        $this->productController = new ProductController();
+        $this->userController = new UserController();
+    }
+
     public function home() {
-        $productController = new ProductController();
-        $data = ['outStandingProducts' => $productController->getTop4OutStandingProducts()];
+        $data = ['outStandingProducts' => $this->productController->getTop4OutStandingProducts()];
         return $this->render('home', $data);
     }
 
     public function signUp() {
         $request = new Request();
         $data = $request->getBody();
-        $userController = new UserController();
-        $message = $userController->signUp($data);
+        $message = $this->userController->signUp($data);
         if ($message['isValid']) {
             return $this->render('signUpSuccess', ['message' => $message]);
         }
