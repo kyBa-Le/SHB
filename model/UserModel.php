21657771e43bf6f413d591bbc2dc5ec8ea542f1a
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-10 15:49:42 +0700

    4 | create user sign up validation - kyba

--- model/UserModel.php
+++ model/UserModel.php
@@ -6,5 +6,9 @@ use app\core\Model;
 
 class UserModel extends Model
 {
-
+    public function getUserByEmail(mixed $email)
+    {
+        $sql = "SELECT * FROM $this->table WHERE email = '$email'";
+        $this->queryOneRow($sql);
+    }
 }
---
+++ validation/UserValidation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\validation;
+
+use app\model\UserModel;
+
+class UserValidation
+{
+
+    private array $message = [];
+    public function __construct()
+    {
+        $this->message['isValid'] = false;
+        $this->message['errors'] = [];
+    }
+    public function validateSignUp($user): array
+    {
+        $errors = array();
+        $userModel = new UserModel();
+        if (empty($user['email'])) {
+            $errors['email'] = 'must not be empty';
+        }
+        if (empty($user['password'])) {
+            $errors['password'] = 'must not be empty';
+        }
+        if (empty($user['username'])) {
+            $errors['name'] = 'must not be empty';
+        }
+        if (empty($user['fullName'])) {
+            $errors['fullName'] = 'must not be empty';
+        }
+        if (!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = 'Invalid email';
+        }
+        if ($userModel->getUserByEmail($user['email']) !== null) {
+            $errors['email'] = 'Email already exists';
+        }
+        if (strlen($user['password']) < 6) {
+            $errors['password'] = 'Password should be at least 6 characters';
+        }
+        if (strlen($user['phone']) !== 10) {
+            $errors['phone'] = 'Must be 10 digits number';
+        }
+        else {
+            $this->message['isValid'] = true;
+        }
+        $this->message['errors'] = $errors;
+
+        return $this->message;
+    }
+
+}
