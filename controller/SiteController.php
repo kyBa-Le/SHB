bafacd70fb60dc1ffd34887ec1e14df1a419d9e5
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-10 16:10:38 +0700

    4 | create sign up function in Site controller - kyba

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -11,4 +11,17 @@ class SiteController extends Controller
         $data = ['outStandingProducts' => $productController->getTop4OutStandingProducts()];
         return $this->render('home', $data);
     }
+
+    public function signUp() {
+        $request = new Request();
+        $data = $request->getBody();
+        $userController = new UserController();
+        $message = $userController->signUp($data);
+        if ($message['isValid']) {
+            return $this->render('signUpSuccess', ['message' => $message]);
+        }
+        else {
+            return $this->render('signUp', ['message' => $message]);
+        }
+    }
 }
