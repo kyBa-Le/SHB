be60d754a8d57f48edf1b93f321564b6289a0767
kimsa-0605 <holykimsa05@gmail.com>
2024-12-12 10:25:27 +0700

    28 | create controller to handle input data and create router login - kimsa

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 use app\controller\SiteController;
 require __DIR__ . "/../vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
@@ -20,8 +20,10 @@ $app = new app\core\Application($config);
 $app->router->get('/', [new SiteController(), 'home']);
 $app->router->get('/sign-up', 'signUp');
 $app->router->get('/sign-up/success', 'signUpSuccess');
+$app->router->get('/login', 'login');
 
 // Post request
 $app->router->post('/sign-up', [new SiteController(), 'signUp']);
+$app->router->post('/login', [new SiteController(), 'login']);
 
 $app->run();
