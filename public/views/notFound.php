a9118d5d4cec9fb2640c8884cdd59edf6c40b347
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-14 08:42:04 +0700

    39 | change not found page 404 - kyba

--- public/views/notFound.php
+++ public/views/notFound.php
@@ -1,1 +1,18 @@
-<h1>Not found</h1>
+<style>
+    #not-found-container {
+        width: 60%;
+        height: 30%;
+        img {
+            height: 400px;
+        }
+    }
+</style>
+<div style="background-color: white; width: 100%; margin-top: 15vh" class="d-flex justify-content-around align-items-center  flex-column">
+    <h1>404</h1>
+    <div id="not-found-container" class="d-flex justify-content-center align-items-center">
+        <img src="https://cdn.svgator.com/images/2024/04/electrocuted-caveman-animation-404-error-page.gif">
+    </div>
+    <h1>Page not found</h1>
+    <p class="text-center">The site you are looking for may not exist or be in maintenance.</p>
+    <a href="/">Please go to SHB</a>
+</div>
