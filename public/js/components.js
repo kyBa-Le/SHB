037e085834298e931a680f764c835368e2cd4d1e
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-15 17:04:04 +0700

    43 | add send and get data function to send data to API - kyba

--- public/js/components.js
+++ public/js/components.js
@@ -6,6 +6,28 @@ export function loadWaiting() {
     };
 }
 
+export async function getData(path) {
+    try {
+        const response = await fetch(path); // Đợi fetch hoàn thành
+        const data = await response.json(); // Đợi JSON được parse
+        return (data);
+    } catch (error) {
+        console.error('Error fetching events:', error);
+    }
+}
+
+export async function sendData(path, data) {
+    const response = await fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    const returnData = await response.json();
+    return (returnData);
+}
+
 export function moneyFormater(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
