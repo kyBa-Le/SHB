f9a13415d5cb17e1c1a41831d867d52b2e0c243b
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-22 13:23:54 +0700

    none | edit sendData function to hide waiting when errors appear - kyba

--- public/js/components.js
+++ public/js/components.js
@@ -23,17 +23,27 @@ export async function getData(path) {
 
 export async function sendData(path, data) {
     loadWaiting();
-    const response = await fetch(path, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    });
-    const returnData = await response.json();
-    hideWaiting();
-    return (returnData);
+    try {
+        const response = await fetch(path, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const returnData = await response.json();
+        return returnData;
+    } catch (error) {
+        console.error('Error occurred while sending data:', error);
+        throw error;
+    } finally {
+        hideWaiting();
+    }
 }
+
 
 export function moneyFormater(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
