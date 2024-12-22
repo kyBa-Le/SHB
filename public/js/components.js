dee1ffea99bec770cf70bfa4b9ef57f853283511
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-22 18:06:12 +0700

    none | update fetch function in component - kyba

--- public/js/components.js
+++ public/js/components.js
@@ -21,8 +21,10 @@ export async function getData(path) {
     }
 }
 
-export async function sendData(path, data) {
-    loadWaiting();
+export async function sendData(path, data, isDisplayWaiting = true) {
+    if (isDisplayWaiting) {
+        loadWaiting();
+    }
     try {
         const response = await fetch(path, {
             method: 'POST',
@@ -36,6 +38,54 @@ export async function sendData(path, data) {
         }
         const returnData = await response.json();
         return returnData;
+    } catch (error) {
+        console.error('Error occurred while sending data:', error);
+        throw error;
+    } finally {
+        hideWaiting();
+    }
+}
+
+export async function updateData (path, data, isDisplayWaiting = false) {
+    if (isDisplayWaiting) {
+        loadWaiting();
+    }
+    try {
+        const response = await fetch(path, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error occurred while sending data:', error);
+        throw error;
+    } finally {
+        hideWaiting();
+    }
+}
+
+export async function deleteData (path, data = null, isDisplayWaiting = false) {
+    if (isDisplayWaiting) {
+        loadWaiting();
+    }
+    try {
+        const response = await fetch(path, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return await response.json();
     } catch (error) {
         console.error('Error occurred while sending data:', error);
         throw error;
