cb38f9e7adb2004670e9cb5a13c47a81c5ef71e4
kyBa-Le <lekyba2000hc@gmail.com>
2024-12-27 12:07:28 +0700

    73 | temp -  send data payment to api - kyba

--- public/js/payment.js
+++ public/js/payment.js
@@ -22,19 +22,48 @@ function renderDistrict(province) {
 
 // xử lý khi ấn nút mua hàng
 document.getElementById('order-btn').addEventListener('click',async function() {
-    let method = this.querySelector('.payment-check:checked').value;
-    let fullName = document.getElementById('full-name').value;
+    let method = document.querySelector('input[name="payment"]:checked').value;    let fullName = document.getElementById('full-name').value;
     let phone = document.getElementById('phone-number').value;
     let province = document.getElementById('province').value;
     let district = document.getElementById('district').value;
     let description = document.getElementById('description').value;
     let totalPrice = document.getElementById('total-price').dataset.value;
     let detailedAddress = document.getElementById('detailed-address').value;
-    let data = {total_cost: totalPrice, description:description, method:method, province:province, district:district, detailed_address:detailedAddress};
+    let data =
+            {   total_cost: totalPrice,
+                description:description,
+                method:method,
+                province:province,
+                district:district,
+                detailed_address:detailedAddress,
+                full_name: fullName,
+                phone: phone
+            };
+    let response;
     if (method === 'Momo') {
-
+        // payment with momo
     } else if(method === 'COD') {
-        let response = await sendData('/api/payments', data);
+        response = await sendData('/api/payments', data);
     }
-
+    if (response['isPaid'] === true) {
+        let orderItems = document.querySelectorAll('.item');
+        let paymentId = response['payment']['id'];
+        for (let item of orderItems) {
+            if (item.dataset.isNew == false) {
+                await patchData('/api/order-items/' + item['id'], {payment_id:paymentId});
+            } else {
+                let dataSet = item.dataset;;
+                let data = {
+                    product_name: dataSet.productName,
+                    quantity: dataSet.quantity,
+                    unit_price: dataSet.unitPrice,
+                    size: dataSet.size,
+                    product_id: dataSet.productId,
+                    product_image_link: dataSet.productImageLink,
+                    product_color: dataSet.productColor
+                }
+                await sendData('/api/order-items', data);
+            }
+        }
+    }
 })
--- public/views/payment.php
+++ public/views/payment.php
@@ -3,7 +3,10 @@
         header('Location: /login');
         exit;
     }
-    $data = $_SESSION['user'];  
+    $data = $_SESSION['user'];
+    if (!isset($orderItems)) {
+        header('Location: /');
+    }
 ?>
 <link rel="stylesheet" href="css/payment.css">
 
@@ -11,17 +14,17 @@
   <div class="payment-method-container">
     <div class="payment-method">
       <h4>Payment Method</h4>
-      <label><input type="radio" name="payment" value="cod" checked class="payment-check"> COD</label>
-      <label><input type="radio" name="payment" value="momo" class="payment-check"> Momo</label>
+      <label><input type="radio" name="payment" value="COD" checked class="payment-check"> COD</label>
+      <label><input type="radio" name="payment" value="Momo" class="payment-check"> Momo</label>
       <form>
-        <input id="full-name" type="text" placeholder="Full Name" value="<?php echo $data['fullName'] ?? '' ?>">
+        <input id="full-name" type="text" placeholder="Full Name" value="<?php echo $data['fullName'] ?? '' ?>" required>
         <input id="phone-number" type="text" placeholder="Phone Number" minlength="10" maxlength="10" required value="<?php echo $data['phone'] ?? '' ?>">
         <div class="form-group form-group-select">
-            <select name="province" id="province">
-                <option value="<?php echo $data['province'] ?? '' ?>"><?php echo $data['province'] ?? '' ?></option>
+            <select name="province" id="province" required>
+                <option disabled value="<?php echo $data['province'] ?? '' ?>"><?php echo $data['province'] ?? '' ?></option>
             </select>
-            <select name="district" id="district">
-                <option value="<?php echo $data['district'] ?? '' ?>"><?php echo $data['district'] ?? '' ?></option>
+            <select name="district" id="district" required>
+                <option disabled value="<?php echo $data['district'] ?? '' ?>"><?php echo $data['district'] ?? '' ?></option>
             </select>
             <input type="text" name="detailed_address" placeholder="Detail address" id="detailed-address">
         </div>
@@ -30,7 +33,7 @@
           <h3>Order Summary</h3>
           <p>Order Total: <span class="money" data-value="<?php echo $totalPrice ?>" id="total-price"><?php echo $totalPrice ?>₫</span></p>
         </div>
-        <button class="btn-buy" id="order-btn">Order</button>
+        <button type="button" class="btn-buy" id="order-btn">Order</button>
       </form>
     </div>
   </div>
@@ -52,7 +55,7 @@
                     $name = $value['product_name'];
                     $color = $value['product_color'];
                     $total = $quantity * $price;
-                    echo "<div class='item'>
+                    echo "<div class='item' data-isNew='false'>
                             <img src='$imageLink' alt='Product'>
                             <div class='details'>
                                 <span class='money'>$price đ</span>
@@ -71,12 +74,14 @@
                 $name = $orderItems['product_name'];
                 $color = $orderItems['product_color'];
                 $total = $quantity * $price;
-                echo "<div class='item'>
+                $size = $orderItems['size'];
+                $product_id = $orderItems['product_id'];
+                echo "<div class='item' data-isNew='true' data-product-name=$name data-quantit=$quantity, data-unit-price=$price, data-size=$size, data-product-id=$product_id, data-image-link=$imageLink, data-product-color=$color>
                         <img src='$imageLink' alt='Product'>
                         <div class='details'>
                             <span class='money'>$price đ</span>
                             <p>$name</p>
-                            <p>$color / M</p>
+                            <p>$color / $size</p>
                             <p>x $quantity</p> 
                         </div>
                         <p>Total: <span> $total đ</span></p>
