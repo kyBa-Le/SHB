58f3cec018ad4c5cb1ebac1c58190598c359564b
kimhien0711 <hiencaok@gmail.com>
2024-12-30 13:51:55 +0700

    100 | create layout dashboard - kimhien

---
+++ public/views/layouts/admin.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/admin/admin-dashboard.css">
+<div class="header">
+    <h1>Admin</h1>
+    <div class="logout">
+        <h3>Logout</h3>
+    </div>
+</div>
+<div class="container-dashboard">
+    <div class="sidebar">
+        <h2>CoreUI</h2>
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Users</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Orders</a></li>
+        </ul>
+    </div>     
+</div>
